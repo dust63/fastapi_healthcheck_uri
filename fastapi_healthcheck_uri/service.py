763fabf5295a0f827106df6b4fab13316077aec7
Romain Jourde <romain.jourde_ext@michelin.com>
2024-06-07 14:55:33 +0200

    Add extra params on HealthCheckUri

--- fastapi_healthcheck_uri/service.py
+++ fastapi_healthcheck_uri/service.py
@@ -1,26 +1,52 @@
 from fastapi_healthcheck.domain import HealthCheckInterface
 from fastapi_healthcheck.service import HealthCheckBase
 from fastapi_healthcheck.enum import HealthCheckStatusEnum
-from typing import List
+from typing import Dict, List
 from requests import get
+
 
 class HealthCheckUri(HealthCheckBase, HealthCheckInterface):
     _tags: List[str]
     _connectionUri: str
     _healthyCode: int
     _unhealthyCode: int
+    _ssl_verify: bool
+    _headers: Dict
+    _timeout: float | None
 
-    def __init__(self, connectionUri: str, alias: str, tags: List[str], healthyCode: int = 200, unhealthyCode: int = 500) -> None:
+    def __init__(
+        self,
+        connectionUri: str,
+        alias: str,
+        tags: List[str],
+        healthyCode: int = 200,
+        unhealthyCode: int = 500,
+        ssl_verify: bool = True,
+        headers: Dict = {},
+        timeout: float | None = None,
+    ) -> None:
         self.setConnectionUri(connectionUri)
         self._alias = alias
         self._tags = tags
         self._healthyCode = healthyCode
         self._unhealthyCode = unhealthyCode
+        self._ssl_verify = ssl_verify
+        self._headers = headers
+        self._timeout = timeout
 
     def __checkHealth__(self) -> bool:
-        res = get(url=self.getConnectionUri(), headers={"User-Agent": "FastAPI HealthCheck"})
-        if res.status_code == self._healthyCode:
-            return HealthCheckStatusEnum.HEALTHY
-        elif res.status_code != self._unhealthyCode:
+        try:
+            res = get(
+                url=self.getConnectionUri(),
+                headers={"User-Agent": "FastAPI HealthCheck"} | self._headers,
+                timeout=self._timeout,
+                verify=self._ssl_verify,
+            )
+            if res.status_code == self._healthyCode:
+                return HealthCheckStatusEnum.HEALTHY
+            if res.status_code != self._unhealthyCode:
+                return HealthCheckStatusEnum.UNHEALTHY
+        except:  # pylint: disable=W0702
             return HealthCheckStatusEnum.UNHEALTHY
+
         return HealthCheckStatusEnum.UNHEALTHY
